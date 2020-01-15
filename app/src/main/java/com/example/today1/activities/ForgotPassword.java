fdf8979fdbe2f0faf1ba35f8f9d88cbf11698674
Ahsaan Ansari <ahsaan.se15@gmail.com>
2020-01-15 11:45:04 +0500

    Initial commit

---
+++ app/src/main/java/com/example/today1/activities/ForgotPassword.java
@@ -0,0 +1,77 @@
+package com.example.today1.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.core.widget.NestedScrollView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.today1.R;
+import com.example.today1.activities.ConfirmPassword;
+import com.example.today1.helper.InputValidation;
+import com.example.today1.sql.DatabaseHelper;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+public class ForgotPassword extends AppCompatActivity {
+
+    private TextInputEditText textInputEditTextEmail;
+    private TextInputLayout textInputLayoutEmail;
+
+    private AppCompatButton appCompatButtonConfirm;
+
+    private InputValidation inputValidation;
+    private DatabaseHelper databaseHelper;
+
+    private NestedScrollView nestedScrollView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+
+
+        textInputEditTextEmail = (TextInputEditText) findViewById(R.id.textInputEditTextEmail);
+        appCompatButtonConfirm = (AppCompatButton) findViewById(R.id.appCompatButtonConfirm);
+        nestedScrollView = (NestedScrollView) findViewById(R.id.nestedScrollView);
+
+        databaseHelper = new DatabaseHelper(this);
+        inputValidation = new InputValidation(this);
+
+        setTitle("Recover password");
+        appCompatButtonConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                verifyFromSQLite();
+            }
+        });
+
+    }
+
+    private void verifyFromSQLite(){
+
+        if (textInputEditTextEmail.getText().toString().isEmpty()){
+            Toast.makeText(this, "Please fill your email", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+        if (databaseHelper.checkUser(textInputEditTextEmail.getText().toString().trim())) {
+            Intent accountsIntent = new Intent(this, ConfirmPassword.class);
+            accountsIntent.putExtra("EMAIL", textInputEditTextEmail.getText().toString().trim());
+            emptyInputEditText();
+            startActivity(accountsIntent);
+        } else {
+            Snackbar.make(nestedScrollView, getString(R.string.error_valid_email_password), Snackbar.LENGTH_LONG).show();
+        }
+    }
+
+    private void emptyInputEditText(){
+        textInputEditTextEmail.setText("");
+    }
+}
